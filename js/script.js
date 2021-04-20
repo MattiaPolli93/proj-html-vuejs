bb0b6b1eaa5bdc84ca4ec8c697030b3f53ef3842
Mattia <matpolli93@gmail.com>
2021-04-20 09:55:31 +0200

    Draft "features" section

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@
 const app = new Vue({
     el: "#app",
     data: {
+        // Navbar data
         navbarSections: ["Home", "Services", "Why Us", "Case Studies", "About", "Blog"],
+        phoneNumber: "(555) 802-1234",
+
+        // Section(s) - Features data
+        featuresText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor placerat luctus. Nullam sit amet ante sed orci convallis gravida et at massa.",
+        consultationText: "Get a Consultation",
     },
     methods: {
 
